f1eacdfff4e9974d28cc5e06366ed9c484f9fdd7
mkassjanski <ankoupl80@gmail.com>
2017-10-13 11:03:29 +0200

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // potrzebne blibliteki
+using namespace std; // ptrzestrzeń nazw std (klasyczna)
+int main() // główna funkcja programu
+{
+  cout << "Mój pierwszy program" << endl; 
+  // cout -> jest poleceniem służącym, do wypisywania na ekranie znaków i zmiennych
+  // koniec linii -> koniec lini
+  return 0; // wysyłanie informacji o zakończeniu programu
+}
