241a117b2e37b2c398960dc0cccfd639ef33f59c
mkassjanski <ankoupl80@gmail.com>
2017-10-13 11:10:10 +0200

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    cout << "Podaj liczbe calkowita: ";
+    cin >> a;
+    cout << "Podaj liczbe rzeczywista: ";
+    cin >> b;
+    cout << "Liczba a = " << a << endl;
+    cout << "Liczba b = " << b << endl;
+    return 0;
+}
