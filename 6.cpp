5595f3322d84f1dd03a29cf81e48b95b5631d169
mkassjanski <ankoupl80@gmail.com>
2017-11-09 21:55:56 +0100

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -8,18 +8,19 @@ int main()
   cin >> ocena;
   switch (ocena)
   {
-  case 1: cout << " brak promocji ";
-   break;
-  case 2:
-  case 3:
-  case 4:
-  case 5:
-   cout << " promocja do nastepnej klasy ";
-   break;
-  case 6:
-   cout << " promocja z ocena celujaca";
-   break;
-  default:
-   cout << "Bledny numer oceny";
+    case 1: 
+       cout << " brak promocji ";
+       break;
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+       cout << " promocja do nastepnej klasy ";
+       break;
+    case 6:
+       cout << " promocja z ocena celujaca";
+       break;
+    default:
+       cout << "Bledny numer oceny";
   }
 }
