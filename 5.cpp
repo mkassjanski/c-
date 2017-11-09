954f735a9eb93475a19423aeaa5e75399278e59f
mkassjanski <ankoupl80@gmail.com>
2017-11-09 21:43:06 +0100

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,15 @@
+switch(operator_sterujacy)
+{
+    case wartosc_1:
+        instrukcja1;
+    break;
+    case wartosc_2:
+        instrukcja2;
+    break;
+    case wartosc_3:
+        instrukcja3;
+    break;
+    default:
+        instrukcja4;
+    break;
+}
